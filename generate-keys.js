9a07902a02d2a8afd4eed74f6c692eb55ffd28d9
Dinglebarry9 <n.harmon@blockchainsolutionshi.com>
2020-04-13 12:12:35 -1000

    Added Key generation and a JS method to generate the JWS

---
+++ generate-keys.js
@@ -0,0 +1,16 @@
+var didAuth = require('@decentralized-identity/did-auth-jose');
+var fs = require('fs');
+
+(async () => {
+
+  const kid = '#key-1';
+  const privKey = await didAuth.EcPrivateKey.generatePrivateKey(kid);
+  const pubKey = privKey.getPublicKey();
+  pubKey.defaultSignAlgorithm = 'ES256K';
+
+
+  fs.writeFileSync('./private.jwk', JSON.stringify(privKey));
+  fs.writeFileSync('./public.jwk', JSON.stringify(pubKey));
+
+})();
+
